58850d10316a0df7c37a6b7378495fa36be4963a
dammuozz <s.damaggio@innoseis.com>
2018-10-25 19:37:52 +0200

    added ContactsItem

--- ashop/src/App.vue
+++ ashop/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    
     <home></home>
+    <contacts></contacts>
   </div>
 </template>
 
 <script>
-import Home from './components/HomeItem'
+import Home from './components/HomeItem.vue'
+import Contacts from './components/ContactsItem.vue'
 
 export default {
   name: 'app',
   components: {
-    Home
+    Home,
+    Contacts
   }
 }
 </script>
---
+++ ashop/src/components/ContactsItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contacts">
+    <h2>Contacts</h2>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'contacts',
+    data() {
+      
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
