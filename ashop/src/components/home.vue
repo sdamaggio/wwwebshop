1d84a88790f693cdc45ee6c9fe750a4e3023fb8a
dammuozz <s.damaggio@innoseis.com>
2018-10-25 18:18:39 +0200

    first commit, created component home, routed it

---
+++ ashop/src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      title: 'Hello home'
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
